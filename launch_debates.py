7510ea6854d0d17ce2321beac2f94e734d50aef1
fabian <fabians@gmail.com>
2023-05-10 11:18:02 +0100

    Autoformat all python files

--- launch_debates.py
+++ launch_debates.py
@@ -1,19 +1,20 @@
 import subprocess
 import time
-import os 
+import os
 
 if __name__ == "__main__":
-    
     t0 = time.time()
-    process_path = os.path.join(os.environ['HOME'], "guanaco", "proc_generate_philosophy_debate.py")
+    process_path = os.path.join(
+        os.environ["HOME"], "guanaco", "proc_generate_philosophy_debate.py"
+    )
     processes = []
-    
-    for i in range(1,4):
-        cfg_path = os.path.join(os.environ['HOME'], "guanaco", f"FredRalph_p{i}.toml")
-        process =  subprocess.Popen(['python3',process_path, cfg_path])
+
+    for i in range(1, 4):
+        cfg_path = os.path.join(os.environ["HOME"], "guanaco", f"FredRalph_p{i}.toml")
+        process = subprocess.Popen(["python3", process_path, cfg_path])
         processes.append(process)
 
     for process in processes:
         process.wait()
-        
-    print('All procs executed, total time elapsed: %.2f'%(time.time() - t0))
+
+    print("All procs executed, total time elapsed: %.2f" % (time.time() - t0))
--- src/ui.py
+++ src/ui.py
@@ -2,8 +2,10 @@ import json
 from typing import Callable, List
 from dataclasses import dataclass, asdict
 
+
 import gradio as gr
 from pyllamacpp.model import Model
+
 
 @dataclass
 class UIState:
@@ -14,18 +16,20 @@ class UIState:
         return json.dumps(asdict(self))
 
 
-def gen_ui(
-        model: Model,
-        initial_prompt: str,
-        exec_round: Callable,
-        cfg
-    ):
-
+def gen_ui(model: Model, initial_prompt: str, exec_round: Callable, cfg):
     def _next_round_btn_on_click(speaker1, speaker2, temperature, state):
         state = UIState(**json.loads(state))
-        prompt, conversation_list = exec_round(model, cfg, state.prompt, state.conversation_list, temperature, speaker1, speaker2)
+        prompt, conversation_list = exec_round(
+            model,
+            cfg,
+            state.prompt,
+            state.conversation_list,
+            temperature,
+            speaker1,
+            speaker2,
+        )
         state = UIState(prompt, conversation_list)
-        return ['\n'.join(state.conversation_list), state.to_json()]
+        return ["\n".join(state.conversation_list), state.to_json()]
 
     with gr.Blocks() as ui:
         initial_state = UIState(prompt=initial_prompt, conversation_list=[])
@@ -33,17 +37,22 @@ def gen_ui(
 
         with gr.Row() as row:
             with gr.Column() as col:
-                speaker1 = gr.Textbox(label='Speaker 1', value=cfg['debate_params']['speaker1_fullname'])
-                speaker2 = gr.Textbox(label='Speaker 2', value=cfg['debate_params']['speaker2_fullname'])
-                temperature = gr.Slider(0, 1, label='Temperature')                
-                next_round_btn = gr.Button('Next round')     
+                speaker1 = gr.Textbox(
+                    label="Speaker 1", value=cfg["debate_params"]["speaker1_fullname"]
+                )
+                speaker2 = gr.Textbox(
+                    label="Speaker 2", value=cfg["debate_params"]["speaker2_fullname"]
+                )
+                temperature = gr.Slider(0, 1, label="Temperature")
+                next_round_btn = gr.Button("Next round")
 
             with gr.Column() as col:
-                output = gr.Textbox(label='Output')
+                output = gr.Textbox(label="Output")
 
             next_round_btn.click(
                 fn=_next_round_btn_on_click,
                 inputs=[speaker1, speaker2, temperature, state],
-                outputs=[output, state])
+                outputs=[output, state],
+            )
 
     return ui
