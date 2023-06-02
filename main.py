e7e094241f5fe343d67a55d8e9841e5a14f51214
taraful <tarafulis@gmail.com>
2023-06-02 19:12:05 +0100

    updates for sequence integration for vicuna and ratio to keep parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src import ui, segments, temperature
+from src.vicuna import segments as vic_segments
 
 from pyllamacpp.model import Model
 from tqdm import tqdm
@@ -17,9 +18,9 @@ logger = logging.getLogger(__name__)
 FLAGS = flags.FLAGS
 flags.DEFINE_string("config", "FredRalph_p1.toml", "Path to the TOML config")
 flags.DEFINE_string("model-path", None, "Path to the GGML model")
-flags.DEFINE_integer(
-    "n-threads", int(os.cpu_count() / 2), "Number of threads to use for running model"
-)
+flags.DEFINE_integer("n-threads", int(os.cpu_count() / 2), "Number of threads to use for running model")
+flags.DEFINE_float("ratio_keep", float(2/3), "Proportion of context to keep in next input")
+flags.DEFINE_enum("model-type","vicuna",["vicuna","llama"], "Class of LLM")
 flags.DEFINE_integer("rounds", None, "Number of rounds to execute")
 flags.DEFINE_string("output", None, "Output file")
 flags.DEFINE_string(
@@ -39,18 +40,31 @@ flags.DEFINE_float("decay-constant", 0.05, "decay constant for the exponential d
 flags.DEFINE_bool("gradio", False, "Whether to spin up a UI or not")
 
 
-def exec_round(model, cfg, prompt, conversation_list, temperature, speaker1, speaker2):
+def exec_round(model, cfg, prompt, ratio_keep, conversation_list, temperature, speaker1, speaker2):
+
     cfg["gpt_params"]["temp"] = temperature
-    output = model.generate(
-        prompt,
+    output = model.generate(prompt,
         **{**cfg["gpt_params"], **{"n_threads": cfg["debate_params"]["n_threads"]}},
     )
-    prompt, conversation_list = segments.get_new_prompt(
-        output,
-        conversation_list,
-        n_keep=int(2 * cfg["model_params"]["n_ctx"] / 3),
-        speakers=[speaker1, speaker2],
-    )
+
+    if FLAGS['model-type'].value == 'llama':
+
+        prompt, conversation_list = segments.get_new_prompt(
+            output,
+            conversation_list,
+            n_keep=int(cfg["model_params"]["n_ctx"]*ratio_keep),
+            speakers=[speaker1, speaker2],
+        )
+
+    elif FLAGS['model-type'].value == 'vicuna':
+
+        prompt, conversation_list = vic_segments.get_new_prompt(
+            output,
+            conversation_list,
+            n_keep=int(cfg["model_params"]["n_ctx"]*ratio_keep),
+            speakers=[speaker1, speaker2],
+        )
+
 
     return prompt, conversation_list
 
